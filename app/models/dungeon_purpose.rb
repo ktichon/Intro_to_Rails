af045a1b7f0548facc47137f3f2bd229320a5fe6
ktichon <ktichon@icould.com>
2021-10-22 12:21:03 -0500

    Added Dungeon Model and Vaildation

--- app/models/dungeon_purpose.rb
+++ app/models/dungeon_purpose.rb
@@ -1,4 +1,4 @@
 class DungeonPurpose < ApplicationRecord
-  has_many :chamber_purposes
+  has_many :chamber_purposes, :dungeon_purposes
   vaildates :purpose, presence:true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_170058) do
+ActiveRecord::Schema.define(version: 2021_10_22_171605) do
 
   create_table "chamber_purposes", force: :cascade do |t|
     t.string "purpose"
@@ -27,5 +27,19 @@ ActiveRecord::Schema.define(version: 2021_10_22_170058) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "dungeons", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "key_event"
+    t.integer "width"
+    t.integer "depth"
+    t.integer "chambers"
+    t.integer "dungeon_purpose_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dungeon_purpose_id"], name: "index_dungeons_on_dungeon_purpose_id"
+  end
+
   add_foreign_key "chamber_purposes", "dungeon_purposes"
+  add_foreign_key "dungeons", "dungeon_purposes"
 end
