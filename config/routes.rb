bb94ecf5563a48150ba9aa6fa3e7612d53e46948
ktichon <ktichon@icould.com>
2021-10-22 15:41:18 -0500

    Added controllers, show, and index for all 4 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'chamber_purposes/index'
+  get 'chamber_purposes/show'
+  get 'dungeon_purposes/index'
+  get 'dungeon_purposes/show'
+  get 'dungeons/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
