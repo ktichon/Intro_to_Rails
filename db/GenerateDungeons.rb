0747a7e6a3a783ff351c874ec22e20be091b9e42
ktichon <ktichon@icould.com>
2021-10-22 11:36:00 -0500

    Added Data Sources

---
+++ db/GenerateDungeons.rb
@@ -0,0 +1,124 @@
+require_relative 'Room'
+
+def validate_Room(room_points, rooms_to_check, maxWidth)
+  if(rooms_to_check.empty?)
+    return true
+  end
+
+  if(room_points[:x1] <= 0 || room_points[:x2] >= maxWidth)
+    return false
+  end
+  rooms_to_check.each do |checkRoom|
+    if(!(room_points[:x1] >= checkRoom.getPoints()[:x2] || room_points[:x2] <= checkRoom.getPoints()[:x1] ||
+      room_points[:y1] >= checkRoom.getPoints()[:y2] || room_points[:y2]  <= checkRoom.getPoints()[:y1]))
+      return false
+    end
+  end
+  return true
+end
+
+def printMap(dungeonStats)
+  maxDepth = dungeonStats[:max_y] + 1
+  maxlong = dungeonStats[:max_x] + 2
+  dungeon = dungeonStats[:all_rooms]
+
+  for row in 0..maxDepth
+    rowline = ""
+    for column in 0..maxlong
+      collumnValue = "#"
+
+      dungeon.each do |checkRoom|
+        if(column >= checkRoom.getPoints()[:x1] && column <= checkRoom.getPoints()[:x2] &&
+          row >= checkRoom.getPoints()[:y1] && row <= checkRoom.getPoints()[:y2])
+          if (collumnValue == '#' || collumnValue == 'p')
+            collumnValue = checkRoom.getType()
+          end
+        end
+     end
+      rowline += collumnValue
+    end
+    puts(rowline)
+  end
+  puts "Deepist part of the dungeon: #{maxDepth}"
+  puts "Widest  part of the dungeon: #{maxlong}"
+  puts "Number of rooms: #{dungeonStats[:num_rooms]}"
+  puts "Number of chambers: #{dungeonStats[:num_chambers]}"
+  puts "Dungeon end condition: #{dungeonStats[:end_reason]}"
+
+end
+
+def create_dungeon(maxWidth, maxRooms, maxChambers, realtive_sizing )
+  midPoint = maxWidth.div(2)
+  adjust = realtive_sizing.div(2)
+  start_Points = {:x1 => midPoint, :y1 => 0, :x2 => midPoint + realtive_sizing, :y2 => 0}
+  exit_zero = {:from_points => start_Points, :direction => 2, :from => "s0" }
+  startingRoom = Room.new(exit_zero, realtive_sizing, adjust, 's')
+
+  valid_rooms = [startingRoom]
+  exits = startingRoom.getExits(valid_rooms.length)
+  numChambers = 0
+  while !exits.empty?
+    if(valid_rooms.length == maxRooms || numChambers == maxChambers )
+      break
+    end
+
+    currentExit = exits.shift().clone
+    roomTypes = ['p','p', 'p', 'c']
+    if(!(currentExit[:from].include? 'p'))
+      roomTypes = ['p']
+    end
+
+
+    new_room_type = roomTypes.sample
+
+
+    new_room = Room.new(currentExit, realtive_sizing, adjust, new_room_type)
+
+    if(validate_Room(new_room.getPoints(), valid_rooms, maxWidth))
+      valid_rooms.push(new_room)
+      exits.push(*new_room.getExits(valid_rooms.length))
+      if(new_room_type == 'c')
+        numChambers += 1
+      end
+    end
+  end
+
+
+  min_x = maxWidth
+  max_x = 0
+  max_y =0
+  valid_rooms.each do |findRoomStats|
+    if(findRoomStats.getPoints()[:x1] < min_x)
+      min_x = findRoomStats.getPoints()[:x1]
+    end
+    if(findRoomStats.getPoints()[:x2] > max_x)
+      max_x = findRoomStats.getPoints()[:x2]
+    end
+    if(findRoomStats.getPoints()[:y2] > max_y)
+      max_y = findRoomStats.getPoints()[:y2]
+    end
+
+  end
+  valid_rooms.each do |resizeRoom|
+    resizeRoom.resizeX(min_x -1)
+  end
+  max_x -= min_x
+
+  dungeon_end_reason = "Dungeon finished because "
+  if(exits.empty?)
+    dungeon_end_reason += "there where no more available exits"
+  elsif(valid_rooms.length == maxRooms)
+    dungeon_end_reason += "it reached the max number of rooms"
+  elsif(numChambers == maxChambers)
+    dungeon_end_reason += "it reached the max number of chambers"
+  else
+    dungeon_end_reason += "of an unkown reason"
+  end
+
+  return { :all_rooms => valid_rooms, :end_reason => dungeon_end_reason, :num_chambers => numChambers, :num_rooms => valid_rooms.length,
+  :min_x => min_x, :max_x => max_x , :max_y => max_y}
+end
+
+printMap(create_dungeon(50, 50, 10, 3))
+
+
---
+++ db/Room.rb
@@ -0,0 +1,96 @@
+class Room
+  def initialize(from_exit, widthFactor, fitDoorsNice, type)
+     @direction = from_exit[:direction]
+     from_points = from_exit[:from_points]
+     @type = type
+     @length =  widthFactor
+     @width = 1
+     @adjustment = 0
+     if(type == 'p' || (from_exit[:from].include? 's'))
+      @adjustment = fitDoorsNice
+     end
+     if(type == 'c')
+      @width = widthFactor
+     end
+     if(type == 's')
+      @width = widthFactor + 2
+      @length += 2
+     end
+
+    #  if (type == 's')
+    #   @width = widthFactor + 1
+    #   @length += 1
+    #  end
+    case @direction
+    when 0
+      @x1 = from_points[:x1] + @adjustment
+      @y1 = from_points[:y1] + @length
+      @x2 = from_points[:x1] + @width + @adjustment
+      @y2 = from_points[:y1]
+    when 1
+      @x1 = from_points[:x2]
+      @y1 = from_points[:y1] + @adjustment
+      @x2 = from_points[:x2] + @length
+      @y2 = from_points[:y2] + @width + @adjustment
+    when 2
+      @x1 = from_points[:x2] - @width - @adjustment
+      @y1 = from_points[:y2]
+      @x2 = from_points[:x2] - @adjustment
+      @y2 = from_points[:y2] + @length
+    when 3
+      @x1 = from_points[:x1] - @length
+      @y1 = from_points[:y2] - @width - @adjustment
+      @x2 = from_points[:x1]
+      @y2 = from_points[:y2] - @adjustment
+
+    else
+      puts "Room:Error, direction not valid"
+    end
+  end
+
+  def getDirection()
+    return @direction
+  end
+
+  def getPoints()
+    return {:x1 => @x1, :y1 => @y1, :x2 => @x2, :y2 => @y2}
+  end
+  def resizeX(resize)
+    @x1 -= resize
+    @x2 -= resize
+  end
+
+  def getType()
+    return @type
+  end
+
+  def getExits(id)
+    num_exit_odds = [1,1,1,2,2,3]
+    if (getType == 'c')
+      num_exit_odds = [1,1,1,2,2,2,3,3,3]
+    end
+    if(getType == 's')
+      num_exit_odds = [2,3,3]
+    end
+    num_exits = num_exit_odds.sample
+    available_exits = [-1, 0, 1]
+    exits = []
+    for i in 0...num_exits
+      random_direction = available_exits.sample
+      available_exits.delete(random_direction)
+      exits.push({:from_points => getPoints, :direction => checkDirection(random_direction + getDirection()), :from => "#{getType}#{id}" })
+    end
+    return exits
+
+  end
+  def checkDirection(dir)
+    if (dir <= -1)
+      dir = 3
+    elsif(dir >= 4)
+      dir = 0
+    end
+    return dir
+  end
+
+end
+
